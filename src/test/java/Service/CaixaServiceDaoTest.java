dc87b97405aea3a036593e046ffbd4404478fff8
kleberton.santos <ti.klebersantos@gmail.com>
2023-05-12 14:45:09 -0300

    Adicionado teste do caixa

--- src/test/java/Service/CaixaServiceDaoTest.java
+++ src/test/java/Service/CaixaServiceDaoTest.java
@@ -25,7 +25,30 @@ public class CaixaServiceDaoTest {
     }
 
     @Test
-    
+    public void deveRetornarPedidoSemPagamento(){
+        Pedido pedido = new Pedido(1,new Date(),10.0);
+        Caixa caixa = new Caixa(1,5.0,0.0, StatusPagamento.PAGO,pedido);
+        caixaService.finalizaPedido(pedido,caixa);
+        Assertions.assertEquals(StatusPagamento.CANCELADO,caixa.getStatus());
+        Assertions.assertEquals(5.0,caixa.getSaldo());
+    }
+    @Test
+    public void deveFInalizarPedidoPagamentoMaiorQueTotal(){
+        Pedido pedido = new Pedido(1,new Date(),100.0);
+        Caixa caixa = new Caixa(1,0.0,150.0, StatusPagamento.PAGO,pedido);
+        caixaService.finalizaPedido(pedido,caixa);
+        Assertions.assertEquals(StatusPagamento.PAGO,caixa.getStatus());
+        Assertions.assertEquals(100.0,caixa.getSaldo());
+    }
+
+    @Test
+    public void deveFinalizarPedidoPagamentoTotal(){
+        Pedido pedido = new Pedido(1,new Date(),100.0);
+        Caixa caixa = new Caixa(1,0.0,150.0, StatusPagamento.PAGO,pedido);
+        caixaService.finalizaPedido(pedido,caixa);
+        Assertions.assertEquals(StatusPagamento.PAGO,caixa.getStatus());
+       
+    }
 
 
 
